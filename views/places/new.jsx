8c4bff6c9c3e321c3ea7eff7567538ff1a7fc288
Jake Filipowski <filipowskijm@gmail.com>
2023-02-25 10:16:05 -0500

    updated new form styling

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -6,6 +6,29 @@ function new_form () {
         <Def>
           <main>
             <h1>Add a New Place</h1>
+            <form method="POST" action="/places">
+            <div className='form-group'>
+                <label htmlFor="name" className='form-control'>Place Name</label>
+                <input  id="name" name="name" required />
+            </div>
+             <div className='form-group'>
+                <label htmlFor="pic" className='form-control' >Place Picture</label>
+                <input id="pic" name="pic" />
+            </div>
+            <div className='form-group'>
+                <label htmlFor="city" className='form-control' >City</label>
+                <input id="city" name="city" />
+            </div>
+            <div className='form-group'>
+                <label htmlFor="state" className='form-control' >State</label>
+                <input id="state" name="state" />
+            </div>
+            <div className='form-group'>
+                <label htmlFor="cuisines" className='form-control' >Cuisines</label>
+                <input id="cuisines" name="cuisines" required />
+            </div>
+            <input className='btn btn-primary' type="submit" value="Add Place" />
+            </form>
           </main>
         </Def>
     )
