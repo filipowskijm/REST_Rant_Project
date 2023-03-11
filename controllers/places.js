09688ac6cac5a46cf49f38359d27ac545779914a
Jake Filipowski <filipowskijm@gmail.com>
2023-03-11 10:20:13 -0500

    comment functionality completed

--- controllers/places.js
+++ controllers/places.js
@@ -30,6 +30,7 @@ router.get('/new', (req, res) => {
 
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
+  .populate('comments')
   .then(place => {
       res.render('places/show', { place })
   })
@@ -39,9 +40,47 @@ router.get('/:id', (req, res) => {
   })
 })
 
-router.put('/:id', (req, res) => {
-  res.send('PUT /places/:id stub')
+router.get('/:id/comment', (req, res) => {
+  console.log(req.body);
+  db.Place.findById(req.params.id)
+    .then(place => {
+      res.render('places/newcomment', { place });
+    })
 })
+
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  req.body.stars = Number(req.body.stars)
+  if (req.body.rant == 'on'){
+    req.body.rant = true
+  }
+  else {
+    req.body.rant = false
+  }
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
+})
+
+// if (req.body.rant == "on") {
+//   req.body.rant = true
+// } else {
+//   req.body.rant = false
+// }
 
 router.delete('/:id', (req, res) => {
   res.send('DELETE /places/:id stub')
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,33 +1,69 @@
 const React = require('react')
 const Def = require('../default')
 
-function show (data) {
+function show ({place}) {
+  let comments = (
+    <h3 className="inactive">
+      No comments yet!
+    </h3>
+  )
+
+  if (place.comments.length) {
+    comments = place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
+
     return (
-        <Def>
-          <main>
-            <h1>{data.place.name}</h1>
-            <img src={data.place.pic} alt={data.place.name}></img>
-            <h2>Rating</h2>
-            <p>Not rated</p>
-            <h2>Description</h2>
-            <p>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</p>
-            <h4>
-              {data.place.showEstablished()}
-            </h4>
-            <h2>Comments</h2>
-            <p>No comments yet!</p>
-            <a href={`places/${data.place.id}/edit`} className="btn btn-warning"> 
-              Edit
-             </a>  
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-            <button type="submit" className="btn btn-danger">
-              Delete
-            </button>
-          </form>     
-          </main>
-        </Def>
+      <Def>
+        <main>
+          <div className="row">
+            <div className="col-sm-6">
+              <h1>{place.name}</h1>
+              <img src={place.pic} alt={place.name} />
+              <h3>
+                Located in {place.city}, {place.state}
+              </h3>
+            </div>
+            <div className="col-sm-6">
+              <h2>Rating</h2>
+              <h3>
+                Not Rated
+              </h3>
+              <h2>
+                Description
+              </h2>
+              <h3>
+                {place.showEstablished()}
+              </h3>
+              <h4>
+                Serving {place.cuisines}
+              </h4>
+              <a href={`/places/${place.id}/edit`} className="btn btn-warning">Edit</a>
+              <form method="POST" action={`/places/${place.id}?_method=DELETE`}> 
+                  <button type="submit" className="btn btn-danger">Delete</button>
+              </form>
+            </div>
+            <div className="col-sm-12">
+                <h2>
+                  Comments
+                </h2>
+                {comments}
+            </div>
+            <a href={`/places/${place.id}/comment`}>Add New Comment</a>
+          </div>
+        </main>
+      </Def>
     )
 }
-
 
 module.exports = show
